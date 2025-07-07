612c0fb7b9db8481907d1026542ad13f9732e3d8
sergeych <real.sergeych@gmail.com>
2025-07-07 15:02:10 +0300

    v.0.7.1 improved imports

--- lynglib/build.gradle.kts
+++ lynglib/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 group = "net.sergeych"
-version = "0.7.0-SNAPSHOT"
+version = "0.7.1-SNAPSHOT"
 
 buildscript {
     repositories {
