af463163bb860f8d48b9c977e1e1b72e88388cfb
sergeych <real.sergeych@gmail.com>
2025-08-04 16:52:25 +0300

    published 0.8.0-SNAPSHOT

--- lynglib/build.gradle.kts
+++ lynglib/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 group = "net.sergeych"
-version = "0.7.4-SNAPSHOT"
+version = "0.8.0-SNAPSHOT"
 
 buildscript {
     repositories {
