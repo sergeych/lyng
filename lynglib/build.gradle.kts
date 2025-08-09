f9198fe583af3dfb5e23cd5592cc1a205b78e3fd
sergeych <real.sergeych@gmail.com>
2025-08-09 15:55:30 +0300

    parallelism and flows + bugfixes release

--- lynglib/build.gradle.kts
+++ lynglib/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 group = "net.sergeych"
-version = "0.8.2-SNAPSHOT"
+version = "0.8.3-SNAPSHOT"
 
 buildscript {
     repositories {
