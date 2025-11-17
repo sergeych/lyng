88f035beb6a618a7ba361df8a1bf1555eb542911
sergeych <real.sergeych@gmail.com>
2025-11-17 15:25:26 +0100

    version bump

--- lynglib/build.gradle.kts
+++ lynglib/build.gradle.kts
@@ -21,7 +21,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 group = "net.sergeych"
-version = "0.11.1-SNAPSHOT"
+version = "1.0.0-SNAPSHOT"
 
 buildscript {
     repositories {
