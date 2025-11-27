26ddb94f5d3804430e2e41d767db4945622f14af
sergeych <real.sergeych@gmail.com>
2025-11-27 21:42:42 +0100

    fixed version number

--- lynglib/build.gradle.kts
+++ lynglib/build.gradle.kts
@@ -21,7 +21,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 group = "net.sergeych"
-version = "1.0.4-SNPASHOT"
+version = "1.0.4-SNAPSHOT"
 
 // Removed legacy buildscript classpath declarations; plugins are applied via the plugins DSL below
 
